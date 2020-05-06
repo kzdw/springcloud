da5a114cf52803bba9996ec99c1fbdf2bc0b08cf
kzdw <449241216@qq.com>
2020-05-06 10:39:56 +0800

    update

--- rabbitmq-demo/src/main/java/com/zhousheng/rabbitmqtest/util/ConnectionUtil.java
+++ rabbitmq-demo/src/main/java/com/zhousheng/rabbitmqtest/util/ConnectionUtil.java
@@ -3,11 +3,6 @@ package com.zhousheng.rabbitmqtest.util;
 import com.rabbitmq.client.Connection;
 import com.rabbitmq.client.ConnectionFactory;
 
-/**
- * 需要咨询java高级VIP课程的同学可以木兰老师的QQ：2746251334
- * 需要往期视频的同学可以加加安其拉老师的QQ：3164703201
- * author：鲁班学院-商鞅老师
- */
 public class ConnectionUtil {
 
 
